701d206a5864c535b456b72958de138f12e420ae
KUNAPAREDDY SUDHA RANI <126180396+Sudharani3@users.noreply.github.com>
2023-07-19 10:37:48 +0530

    Add files via upload

---
+++ flower_script.js
@@ -0,0 +1,12 @@
+function phase1()
+{
+  document.getElementById("butterfly").src="https://cdn.pixabay.com/photo/2022/09/05/18/53/caterpillar-7434958_1280.jpg";
+}
+function phase2()
+{
+  document.getElementById("butterfly").src="https://i.pinimg.com/originals/49/c3/44/49c34486a46d25df0451a0c88a1f0519.jpg";
+}
+function phase3()
+{
+  document.getElementById("butterfly").src="https://thumbs.dreamstime.com/b/beautiful-pink-butterfly-isolated-white-background-130442371.jpg";
+}
